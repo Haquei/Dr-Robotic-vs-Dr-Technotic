faab747ce890fae1f79d014235218424caf5b990
peyvand-pournasir <peyvandpournasir.pp@gmail.com>
2022-07-16 13:53:50 +0430

    MachineGunTower1stDesign
    it still needs the model for bullet and the tower itself,I made the range public so you can change it anytime.
    *added a tag "Enemy" so the tower only shoot the bullet to the objects with this tag.
    *I still need to code the specific shoot order

---
+++ Assets/Scripts/MGTower.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MGTower : MonoBehaviour
+{
+    private Transform Target;
+
+    [Header("Attributes")]
+
+    public float Range;
+    public float FireRate = 1f;
+    private float FireCountDown = 0f;
+
+
+
+    [Header("UnityField")]
+
+    public string EnemyTag = "Enemy";
+    public GameObject BulletPrefab;
+    public Transform FirePoint;
+    void Start()
+    {
+        InvokeRepeating("UpdateTarget", 0f, 0.5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Target == null)
+        {
+            return;
+        }
+        if (FireCountDown <= 0f)
+        {
+            Shoot();
+            FireCountDown = 1f / FireRate;
+        }
+        FireCountDown -= Time.deltaTime;
+    }
+    void UpdateTarget()
+    {
+        GameObject[] Enemies = GameObject.FindGameObjectsWithTag(EnemyTag);
+        float ShortestDistance = Mathf.Infinity;
+        GameObject NearestEnemy = null;
+
+        foreach (GameObject Enemy in Enemies)
+        {
+            float DistanceToEnemy = Vector3.Distance(transform.position, Enemy.transform.position);
+            if (DistanceToEnemy < ShortestDistance)
+            {
+                ShortestDistance = DistanceToEnemy;
+                NearestEnemy = Enemy;
+
+            }
+
+
+        }
+        if (NearestEnemy != null && ShortestDistance <= Range)
+        {
+            Target = NearestEnemy.transform;
+
+        }
+        else
+        {
+            Target = null;
+        }
+
+
+    }
+    void Shoot()
+    {
+
+
+        if (Target != null)
+        {
+            GameObject BulletGO = (GameObject)Instantiate(BulletPrefab, FirePoint.position, FirePoint.rotation);
+            MGTowerbullet bullet = BulletGO.GetComponent<MGTowerbullet>();
+            if (bullet != null)
+            {
+                bullet.Chase(Target);
+            }
+        }
+
+
+
+    }
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, Range);
+    }
+}
---
+++ Assets/Scripts/MGTowerbullet.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MGTowerbullet : MonoBehaviour
+{
+    private Transform BulletTarget;
+    public float Speed = 10f;
+
+    public void Chase(Transform _BulletTarget)
+    {
+        BulletTarget = _BulletTarget;
+    }
+    void Update()
+    {
+        if (BulletTarget == null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Vector3 dir = BulletTarget.position - transform.position;
+        float DistanceThisFrame = Speed * Time.deltaTime;
+
+
+        if (dir.magnitude <= DistanceThisFrame)
+        {
+            HitTarget();
+            return;
+        }
+
+        transform.Translate(dir.normalized * DistanceThisFrame, Space.World);
+
+    }
+
+    void HitTarget()
+    {
+
+    }
+}
